1ee899b6dd8a6f048d19968a361845d26fb775be
taffy <ececanous@gmail.com>
2025-08-25 10:42:58 +1000

    [SCRUM-81] resolved required issues

--- __tests__/DatePicker.test.tsx
+++ __tests__/DatePicker.test.tsx
@@ -4,7 +4,7 @@ import DatePicker from '@/components/DatePicker';
 import dayjs from 'dayjs';
 import { jest } from '@jest/globals';
 
-const setup = (props = {}) => {
+const renderDatePicker = (props = {}) => {
   const onChange = jest.fn();
   const utils = render(
     <DatePicker value={dayjs()} onChange={onChange} label="Pick a date" {...props} />,
@@ -17,27 +17,30 @@ const setup = (props = {}) => {
 
 describe('DatePicker component', () => {
   test('Renders with label', () => {
-    setup();
+    renderDatePicker();
 
     const labels = screen.getAllByText('Pick a date');
     expect(labels.length).toBeGreaterThan(0);
   });
 
   test('Disables the input when disabled is true', () => {
-    setup({ label: 'Disabled Date', disabled: true });
+    renderDatePicker({ label: 'Disabled Date', disabled: true });
 
     const group = screen.getByRole('group', { name: /Disabled Date/i });
     expect(group).toHaveClass('Mui-disabled');
   });
 
   test('Calls onChange with expected value', () => {
-    const { onChange, container } = setup();
+    const { onChange, container } = renderDatePicker();
 
     const input = container.querySelector('input');
     expect(input).not.toBeNull();
 
     fireEvent.change(input!, { target: { value: '2025-08-20' } });
 
-    expect(onChange).toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith(
+      null,
+      expect.objectContaining({ validationError: null }))
   });
+
 });
