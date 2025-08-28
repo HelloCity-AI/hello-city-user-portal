52aa3975d3fe04237047fcdcaf8b9c3a3259e48f
taffy <ececanous@gmail.com>
2025-08-28 14:21:46 +1000

    [SCRUM-81] Just another day wasting on dependencies issues

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import DatePicker from '@/components/DatePicker';
 import dayjs from 'dayjs';
-import { t } from '@lingui/macro';
+import { i18n } from '@lingui/core';
 
 const meta: Meta<typeof DatePicker> = {
   title: 'Components/DatePicker',
@@ -14,6 +14,6 @@ type Story = StoryObj<typeof DatePicker>;
 export const Default: Story = {
   render: () => {
     const [value, setValue] = useState<dayjs.Dayjs | null>(dayjs());
-    return <DatePicker label={t`Pick a date`} value={value} onChange={setValue} />;
+    return <DatePicker label={i18n._('Pick a date')} value={value} onChange={setValue} />
   },
 };
