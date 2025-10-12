f7c1b9d22dfe32c119bd4186996e86d22ac1875f
Leon Xia <huachi1990@hotmail.com>
2025-10-13 01:24:15 +1100

    [86d1h1h1h] : update next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
   // AWS Amplify动态APP_BASE_URL配置
   // 优先级: 手动配置(自定义域名) > Amplify自动生成 > 本地默认值
   env: {
-    APP_BASE_URL: process.env.APP_BASE_URL ||
+    APP_BASE_URL:
+      process.env.APP_BASE_URL ||
       (process.env.AWS_APP_ID
         ? `https://${process.env.AWS_BRANCH}.${process.env.AWS_APP_ID}.amplifyapp.com`
         : 'http://localhost:3000'),
