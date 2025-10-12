932dfbdbae645056b7ff7e61a03ddc20618f50f2
Leon Xia <huachi1990@hotmail.com>
2025-10-13 01:23:33 +1100

    [86d1h1h1h] : update next config

--- next.config.js
+++ next.config.js
@@ -6,10 +6,12 @@ const nextConfig = {
     // swcPlugins: [['@lingui/swc-plugin', {}]],
   },
   // AWS Amplify动态APP_BASE_URL配置
+  // 优先级: 手动配置(自定义域名) > Amplify自动生成 > 本地默认值
   env: {
-    APP_BASE_URL: process.env.AWS_APP_ID
-      ? `https://${process.env.AWS_BRANCH}.${process.env.AWS_APP_ID}.amplifyapp.com`
-      : process.env.APP_BASE_URL || 'http://localhost:3000',
+    APP_BASE_URL: process.env.APP_BASE_URL ||
+      (process.env.AWS_APP_ID
+        ? `https://${process.env.AWS_BRANCH}.${process.env.AWS_APP_ID}.amplifyapp.com`
+        : 'http://localhost:3000'),
   },
   images: {
     remotePatterns: [
